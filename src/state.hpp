08803fae2fdae5316a8088b60c0b921f8160472a
Pineapple-Soup <adithyaiyer412@gmail.com>
2024-05-03 20:00:45 -0700

    Added cost variable to State class

--- src/state.hpp
+++ src/state.hpp
@@ -8,18 +8,21 @@ class State {
         std::vector<std::vector<int>> data;
         State* parent;
         int depth;
+        int cost;
     public:
-        State(const std::vector<std::vector<int>>& data) : data(data), parent(nullptr), depth(0) {};
-        State(const std::vector<std::vector<int>>& data, State* parent, int depth) : data(data), parent(parent), depth(depth) {};
+        State(const std::vector<std::vector<int>>& data) : data(data), parent(nullptr), depth(0), cost(0) {};
+        State(const std::vector<std::vector<int>>& data, State* parent, int depth) : data(data), parent(parent), depth(depth), cost(0) {};
         State(const State& state);
         State& operator=(const State& rhs);
         ~State();
         std::vector<std::vector<int>> getData() const;
         State* getParent() const;
         int getDepth() const;
+        int getCost() const;
         void setData(std::vector<std::vector<int>>& data);
         void setParent(State* parent);
         void setDepth(int depth);
+        void setCost(int cost);
         std::pair<int, int> findBlank() const;
 };
 
