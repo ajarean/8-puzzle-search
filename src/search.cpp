4a94a86414f8a28290f04a811ab44cbb36c96634
zenxha <komays5042@gmail.com>
2024-05-03 20:43:30 -0700

    Finish heuristic for misplaced tile

--- src/search.cpp
+++ src/search.cpp
@@ -1,9 +1,32 @@
 #include "search.hpp"
 
+
 Search::Search(const Problem& p) {
     this->problem = p;
+}
 
+int MisplacedTileInformedSearch::heuristic(const State& currentState) {
+    int totalHeuristic = 0;
+    std::vector<std::vector<int>> goalState = problem.getGoalState().getData();
+    for(int y = 0; y<goalState.size(); y++) {
+        for(int x = 0; x<goalState[0].size(); x++) {
+            int value = goalState[y][x];
+            totalHeuristic += calculateManhattan(indexInFinal(value, problem), std::make_pair(y, x));
+        }
+    }
+    return totalHeuristic;
+}
 
-int MisplacedTileInformedSearch::heuristic(const State&) {
+int calculateManhattan(std::pair<int, int> value1, std::pair<int, int> value2) {
+    return std::abs(value1.first - value2.first) + std::abs(value2.second - value2.second);
+}
 
+std::pair<int, int> indexInFinal(int num, Problem problem) {
+    for(int y = 0; y<problem.getGoalState().getData().size(); y++) {
+        for(int x = 0; x<problem.getGoalState().getData()[0].size(); x++) {
+            if (problem.getGoalState().getData()[y][x] == num) {
+                return std::make_pair(y, x);
+            }
+        }
+}
 }
--- src/search.hpp
+++ src/search.hpp
@@ -1,14 +1,27 @@
 #ifndef STATE_H
 #define STATE_H
 #include "problem.hpp"
+#include <cstdlib>
 
 class Search {
     private:
         virtual int heuristic(const State&) = 0;
+
+    protected:
         Problem problem;
+        
     public:
+        
         Search(const Problem&);
         State doSearch(const Problem&);
+        friend std::pair<int, int> indexInFinal(int , Problem& );
+};
+
+class MisplacedTileInformedSearch: public Search {
+    private:
+        int heuristic(const State&);
+    
+    public:
 };
 
 #endif
