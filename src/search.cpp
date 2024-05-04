25bccf17cec7b504ef49d6438417831e3d2a7f78
zenxha <komays5042@gmail.com>
2024-05-04 16:48:22 -0700

    fix misplaced heuristic

--- src/search.cpp
+++ src/search.cpp
@@ -7,11 +7,12 @@
 
 std::pair<int, int> Search::indexInFinal(int num) {
     std::pair<int, int> f = {-1, -1};
-    for(int y = 0; y<problem->getGoalState().getData().size(); y++) {
-        for(int x = 0; x<problem->getGoalState().getData()[0].size(); x++) {
-            if (problem->getGoalState().getData()[y][x] == num) {
-                f.first = y;
-                f.second = x;
+    std::vector<std::vector<int>> goalState = problem->getGoalState().getData();
+    for(int y = 0; y<goalState.size(); y++) {
+        for(int x = 0; x<goalState[0].size(); x++) {
+            if (goalState[x][y] == num) {
+                f.first = x;
+                f.second = y;
                 return f;
             }
         }
@@ -43,23 +44,36 @@ std::optional<State> Search::doSearch() {
 
 // Misplaced Tile
 
-int MisplacedTileInformedSearch::heuristic(const State& currentState) {
+int MisplacedTileInformedSearch::heuristic(const State& c) {
     int totalHeuristic = 0;
     std::vector<std::vector<int>> goalState = problem->getGoalState().getData();
-    for(int y = 0; y<goalState.size(); y++) {
-        for(int x = 0; x<goalState[0].size(); x++) {
-            int value = goalState[y][x];
+    std::vector<std::vector<int>> currentState = c.getData();
+    for(int y = 0; y<currentState.size(); y++) {
+        for(int x = 0; x<currentState[0].size(); x++) {
+            // Run distance test on every tile
+            int value = currentState[x][y];
             std::pair coordsFinal = indexInFinal(value);
-            totalHeuristic += calculateManhattan(coordsFinal, std::make_pair(y, x));
+            // std::cout << "  Calculating tile " << value << std::endl;
+            totalHeuristic += calculateManhattan(std::make_pair(x, y), coordsFinal);
+            
         }
     }
+    // std::cout << "      state heuristic of " << totalHeuristic << std::endl;
     return totalHeuristic;
 }
 
 
 
 int MisplacedTileInformedSearch::calculateManhattan(std::pair<int, int> value1, std::pair<int, int> value2) {
-    return std::abs(value1.first - value2.first) + std::abs(value2.second - value2.second);
+    // value1 --> current tile placement
+    // value2 --> goal tile placement
+    // std::cout << "       Finding distance between " << "(" << value1.second <<" , " << value1.first << ") and "  << "(" << value2.second << ", " << value2.first << ")" << std::endl;
+    int dx = std::abs(value1.first - value2.first);
+    int dy = std::abs(value1.second - value2.second);
+    // std::cout << "       dx: " << dx << std::endl;
+    // std::cout << "       dy: " << dy << std::endl;
+    // std::cout << "       tile heuristic " << x << std::endl;
+    return dx+dy;
 }
 
 
