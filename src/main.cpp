6c8ce17063dda76a55b309c8f437c4edd262481a
twu148 <twu148@ucr.edu>
2024-05-04 22:54:40 -0700

    linked github repo

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include "search.hpp"
 #include "menu.hpp"
 
+/**
+ * 
+ *  LINK TO THE GITHUB REPO: https://github.com/ajarean/8-puzzle-search
+ * 
+ *  TEAM MEMBERS:
+ *  Komay Sugiyama (ksugi014)
+ *  Adithya IYer (aiyer026)
+ *  Andy Jarean (ajare002)
+ *  Tingxuan Wu (twu148)
+ * 
+ * 
+*/
+
 int main(int argc, char* argv[]) {
     int choice = 1;
     while (choice==1) {
